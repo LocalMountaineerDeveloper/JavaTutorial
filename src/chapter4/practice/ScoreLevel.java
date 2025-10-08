f19490e31a1a39c753e3ff6ae0bec12fef46b7ed
QKenRu <Myli20(*)20>
2025-10-08 23:30:46 +0800

    Test account

---
+++ src/chapter4/practice/ScoreLevel.java
@@ -0,0 +1,31 @@
+package chapter4.practice;
+
+import java.util.Scanner;
+
+public class ScoreLevel
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("请输入您的分数：");
+        int score=sc.nextInt();
+        if(score>=90 && score<=100)
+        {
+            System.out.println("您的分数"+score+"对应的等级为“优秀”");
+        }else if(score>=80 && score<90)
+        {
+            System.out.println("您的分数"+score+"对应的等级为“良好”");
+        }else if(score>=60 && score<80)
+        {
+            System.out.println("您的分数"+score+"对应的等级为“合格”");
+        }else if(score<60 && score>=0)
+        {
+            System.out.println("您的分数"+score+"对应的等级为“不合格”");
+        }else
+        {
+            System.out.println("无效成绩");
+        }
+        sc.close();
+    }
+}
+
