35db20f8eb195dbbc7e50204e3f46a463e239840
QKenRu <scard713@gmail.com>
2025-10-24 01:38:55 +0800

    Complete chapter10.

---
+++ src/chapter10/practice/ReverseStr.java
@@ -0,0 +1,24 @@
+/*
+* 转置字符串
+* */
+
+package chapter10.practice;
+
+import java.util.Scanner;
+
+public class ReverseStr
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("请输入任意字符串：");
+        String target=sc.next();
+        char[] chr=target.toCharArray();
+        System.out.println("转置后的字符串：");
+        for(int i=chr.length-1;i>=0;i--)
+        {
+            System.out.print(chr[i]);
+        }
+        sc.close();
+    }
+}
